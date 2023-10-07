724ccf8b49d0abccfefe1b5cb546924c6fe551ea
Miguel Cobian <miguelcobian99@gmail.com>
2023-10-06 19:35:36 -0600

    Reading arguments from URL with Router

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,6 +1,19 @@
-
+import { useParams, Navigate } from 'react-router-dom';
+import { getHeroById } from '../helpers';
 export const HeroPage = () => {
+
+    const { id } = useParams();
+
+    const hero = getHeroById(id);
+
+    if (!hero) {
+        return <Navigate to='/marvel' />
+    }
+
     return (
-        <h1>Hero Page</h1>
+        <>
+            <h1>Hero Page</h1>
+
+        </>
     )
 }
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -30,7 +30,7 @@ const router = createBrowserRouter([
                 element: <SearchPage />,
             },
             {
-                path: 'hero',
+                path: 'hero/:id',
                 element: <HeroPage />,
             },
         ]
