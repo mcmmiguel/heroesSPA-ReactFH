d9e08843b5d8c98f3eac8224ba8a684537c050ba
Miguel Cobian <miguelcobian99@gmail.com>
2023-10-06 19:19:59 -0600

    Feat. Improving HeroCard

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -1,7 +1,9 @@
+import { Link } from "react-router-dom";
 
 export const HeroCard = ({ hero }) => {
 
     const heroImageUrl = `../../../assets/heroes/${hero.id}.jpg`;
+
 
     return (
         <div className="col">
@@ -15,7 +17,19 @@ export const HeroCard = ({ hero }) => {
                         <div className="card-body">
                             <h5 className="card-title">{hero.superhero}</h5>
                             <p className="card-text">{hero.alter_ego}</p>
-                            <p className="card-text">{hero.characters}</p>
+
+                            {
+                                (hero.alter_ego !== hero.characters) && <p className="card-text">{hero.characters}</p>
+                            }
+
+                            <p className="card-text">
+                                <small className="text-muted">{hero.first_appearance}</small>
+                            </p>
+
+                            <Link to={`/hero/${hero.id}`}>
+                                Más...
+                            </Link>
+
                         </div>
                     </div>
 
