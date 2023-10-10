79fa3d43a9db5d3585fae0e8e9180b113eadc687
Miguel Cobian <miguelcobian99@gmail.com>
2023-10-10 12:33:18 -0600

    Tests by Fer in authReducer

--- __tests__/auth/context/authReducer.test.js
+++ __tests__/auth/context/authReducer.test.js
@@ -4,7 +4,7 @@ import { types } from "../../../src/auth/types/types";
 
 describe('Pruebas en authReducer', () => {
 
-    let initialState = {};
+    // let initialState = {};
 
     // beforeEach(() => {
     //     initialState = {};
@@ -12,35 +12,53 @@ describe('Pruebas en authReducer', () => {
 
     test('Debe de retornar el estado por defecto', () => {
 
-        const newState = authReducer(initialState, {});
-        expect(newState).toEqual({});
+        const newState = authReducer({ logged: false }, {});
+        expect(newState).toEqual({ logged: false });
 
     });
 
     test('Debe de llamar el Login autenticar y establecer el user', () => {
 
-        const user = { id: 'abc', user: 'Miguel' };
+        // const user = { id: 'abc', user: 'Miguel' };
 
         const action = {
             type: types.login,
-            payload: user,
+            payload: {
+                id: 'abc',
+                name: 'Miguel',
+            },
         };
 
-        const newState = authReducer(initialState, action);
-        expect(newState.user).toEqual(user);
+        const newState = authReducer({ logged: false }, action);
+        expect(newState).toEqual({
+            logged: true,
+            user: action.payload,
+        });
         expect(newState.logged).toBeTruthy();
 
     });
 
     test('Debe de llamar el logout y borrar el user además de establecer logged en false', () => {
 
+        const state = {
+            logged: false,
+            user: {
+                id: '123',
+                name: 'Migue',
+            }
+        }
+
         const action = {
             type: types.logout,
         };
 
-        const newState = authReducer(initialState, action);
-        expect(newState.user).toBeFalsy();
-        expect(newState.logged).toBeFalsy();
+        const newState = authReducer(state, action);
+        expect(newState).toEqual({
+            logged: false,
+        })
+
+        // expect(newState.user).toBeFalsy();
+        // expect(newState.logged).toBeFalsy();
 
     });
 
